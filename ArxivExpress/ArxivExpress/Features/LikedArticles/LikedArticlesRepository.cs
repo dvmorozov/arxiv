a883cbaca1c7714bb1e0fc92985702b3006a1977
Dmitry Morozov <dvmorozov@hotmail.com>
2021-01-09 23:53:02 +0100

    Liked articles are stored to and restored from XML-file.
    #8

--- ArxivExpress/ArxivExpress/Features/LikedArticles/LikedArticlesRepository.cs
+++ ArxivExpress/ArxivExpress/Features/LikedArticles/LikedArticlesRepository.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
-using PCLAppConfig;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace ArxivExpress.Features.LikedArticles
 {
@@ -7,11 +10,52 @@ namespace ArxivExpress.Features.LikedArticles
     {
         private static LikedArticlesRepository _instance;
         private static List<string> _articleIds;
+        private const string _fileName = "liked_articles.xml";
+        private const string _rootElementName = "LikedArticleList";
+        private const string _articleElementName = "LikedArticle";
 
         protected LikedArticlesRepository()
         {
         	_articleIds = new List<string>();
-            var test = ConfigurationManager.AppSettings["config.text"];
+            LoadArticeIds();
+        }
+
+        private void LoadArticeIds()
+        {
+            _articleIds.Clear();
+
+            var filePath = Path.Combine(Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData), _fileName);
+
+            if (File.Exists(filePath))
+            {
+                var xml = XDocument.Load(filePath);
+                var query = from likedArticle
+                            in xml.Root.Descendants(_articleElementName)
+                            select likedArticle.Attribute("id").Value;
+
+                foreach (var articleId in query)
+                {
+                    _articleIds.Add(articleId);
+                }
+            }
+        }
+
+        private void SaveArtcleIds()
+        {
+            var filePath = Path.Combine(Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData), _fileName);
+
+            var xml = new XDocument();
+            var likedArticleElements = new XElement[_articleIds.Count];
+
+            for (var i = 0; i < _articleIds.Count; i++)
+            {
+                likedArticleElements[i] = new XElement(_articleElementName,
+                    new XAttribute("id", _articleIds[i]));
+            }
+            xml.Add(new XElement(_rootElementName, likedArticleElements));
+            xml.Save(filePath);
         }
 
         public static LikedArticlesRepository GetInstance()
@@ -27,6 +71,7 @@ namespace ArxivExpress.Features.LikedArticles
         public void AddArticle(string id)
         {
             _articleIds.Add(id);
+            SaveArtcleIds();
         }
 
         public void DeleteArticle(string id)
@@ -34,6 +79,7 @@ namespace ArxivExpress.Features.LikedArticles
             if (_articleIds.Contains(id))
             {
                 _articleIds.Remove(id);
+                SaveArtcleIds();
             }
         }
 
