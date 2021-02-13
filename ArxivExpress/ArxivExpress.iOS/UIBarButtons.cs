aec4ce2fb778d5e4900824d282ddf26d454219a0
Dmitry Morozov <dvmorozov@hotmail.com>
2021-02-13 21:58:20 +0100

    TextChanged handler has been added, missed sources have been added.
    #12

---
+++ ArxivExpress/ArxivExpress.iOS/UIBarButtons.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UIKit;
+
+namespace ArxivExpress.iOS
+{
+    public class UIBarButtons
+    {
+        private UIBarButtonItem[] _uiBarButtons;
+
+        public UIBarButtons()
+        {
+            var doneButton = new UIBarButtonItem(UIBarButtonSystemItem.Done, delegate
+            {
+            });
+
+            _uiBarButtons = new UIBarButtonItem[] {
+                new UIBarButtonItem (UIBarButtonSystemItem.FlexibleSpace),
+                doneButton
+            };
+        }
+    }
+}
