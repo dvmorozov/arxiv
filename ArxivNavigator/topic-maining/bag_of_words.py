5e2876187cd990a3d40d7b799ff417deb3862b66
Dmitry Morozov <dvmorozov@hotmail.com>
2022-11-19 18:05:40 +0100

    Basic text processing has been implemented.
    #109

--- ArxivNavigator/topic-maining/bag_of_words.py
+++ ArxivNavigator/topic-maining/bag_of_words.py
@@ -5,10 +5,66 @@
 ########################################################################################################################
 
 
-def get_bag_of_words_from_file(file_path):
+from gensim import *
+from pprint import pprint  # pretty-printer
+from collections import defaultdict
+
+
+def read_file(file_path):
     file = open(file_path, 'r')
-    data = file.read()
-    print(data)
+    return file.read()
+
+
+def filter_out_stop_words(words):
+    # Removes common words and tokenizes.
+    stop_list = set('for a of the and to in'.split()).union(parsing.preprocessing.STOPWORDS)
+    return [word for word in words if word not in stop_list]
+
+
+def trim(words):
+    for i in range(1, len(words)):
+        words[i] = words[i].strip(',:;.≤≥-+=?!()[]{}')
+
+    # Removes empty words.
+    return [word for word in words if len(word) > 0]
+
+
+def filter_out_rare_words(words):
+    frequency = defaultdict(int)
+
+    for word in words:
+        frequency[word] += 1
+
+    # Removes words that appear only once.
+    return [word for word in words if frequency[word] > 1]
+
+
+def filter_out_short_words(words):
+    return [word for word in words if len(word) > 3]
+
+
+def do_preprocessing(text):
+    words = text.lower().split()
+    print('Original words count', '=>', len(words))
+
+    words = trim(words)
+    print('After trimming words', '=>', len(words))
+
+    words = filter_out_stop_words(words)
+    print('After filtering out stop words', '=>', len(words))
+
+    words = filter_out_rare_words(words)
+    print('After filtering out rare words', '=>', len(words))
+
+    words = filter_out_short_words(words)
+    print('After filtering out short words', '=>', len(words))
+    return words
+
+
+def get_bag_of_words_from_file(file_path):
+    text = read_file(file_path)
+    words = do_preprocessing(text)
+    print(words)
 
 
 if __name__ == '__main__':
